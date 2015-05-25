8f23109b772615a77a541bee130ec8ad7b16c2dc
Zach Riggle <riggle@google.com>
2015-05-24 23:53:12 -0400

    Initial commit

---
+++ home/ctf/.bash_history
@@ -0,0 +1,10 @@
+cd
+ls
+touch foo
+ls -la
+rm -rf foo
+rm -rf .
+cd ..
+rm -rf ctf
+cd
+ls
---
+++ root/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+apt-get install git qemu qemu-user qemu-user-static binfmt
+apt-get install libc6-i386 libc6-armhf-cross
+
+for user in eliza imap wdub justify; do
+  useradd -m $user
+  passwd  -l $user
+  chown -R $user:$user /home/$user
+  chown    root:$user  /home/$user
+  chmod 1770 /home/$user
+  chmod +x /home/$user/$user
+done
+
+pushd /root
+wget http://launchpadlibrarian.net/158174573/libpcre3_8.31-2ubuntu2_armhf.deb
+wget http://launchpadlibrarian.net/185771211/libglib2.0-0_2.40.2-0ubuntu1_armhf.deb
+dpkg -x libpcre3_8.31-2ubuntu2_armhf.deb out
+dpkg -x libglib2.0-0_2.40.2-0ubuntu1_armhf.deb out
+cp out/lib/arm-linux-gnueabihf/* /usr/arm-linux-gnueabihf/lib
+popd
+
